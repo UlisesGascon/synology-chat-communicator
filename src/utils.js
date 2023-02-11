6aa4ca093d251de6d23ab14e55251f5d9c71750c
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-02-11 22:38:29 +0100

    feat: added general utils
    - Added SSL Disable function
    - Added HTTP request handlers
    - Added URL generators
    - Added structure utils for arrays

---
+++ src/utils.js
@@ -0,0 +1,55 @@
+const got = require('got')
+const debug = require('debug')('synology-chat-communicator')
+
+const disableSslRejections = () => {
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+  debug('Environmental variable NODE_TLS_REJECT_UNAUTHORIZED has been disabled')
+}
+
+const ensureArrayStructure = users => Array.isArray(users) ? users : [users]
+
+const generateMessagePayload = ({ users, text, mediaLink }) => {
+  const payload = { user_ids: users, text }
+  debug('Preparing message with basic payload ', { users, text })
+
+  if (mediaLink) {
+    payload.file_url = mediaLink
+    debug('Adding file_url to the message', mediaLink)
+  }
+
+  return JSON.stringify(payload)
+}
+const implementedMethods = ['chatbot', 'user_list', 'channel_list']
+
+const getUrlByMethod = (baseUrl, token) => method => {
+  if (!baseUrl || !token) {
+    throw new Error('The baseUrl or the token are missing. Check out the documentation!')
+  }
+
+  if (!implementedMethods.includes(method)) {
+    throw new Error('Invalid method!')
+  }
+
+  return `${baseUrl}/webapi/entry.cgi?api=SYNO.Chat.External&method=${method}&version=2&token=%22${token}%22`
+}
+
+const makeRequest = method => async (url, settings = {}) => {
+  debug('Starting the request to Synology Chat with url', url)
+  const message = await got[method](url, settings).json()
+
+  if (!message.success) {
+    throw new Error('Request not send', message)
+  }
+
+  debug('Received response from Synology Chat Request', message)
+  return message
+}
+
+module.exports = {
+  disableSslRejections,
+  ensureArrayStructure,
+  generateMessagePayload,
+  getUrlByMethod,
+  makePostRequest: makeRequest('post'),
+  makeGetRequest: makeRequest('get')
+}
